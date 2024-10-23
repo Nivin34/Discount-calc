a98c0d323b3ba86c5e41a88307d9ecfced2b852f
Nivin34 <nivinselvaraj@gmail.com>
2024-10-23 20:35:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let purchaseAmount = document.getElementById("amount");
+let purchaseLimit = document.getElementById("limit");
+let purchaseDiscount = document.getElementById("discount");
+let enter = document.getElementById("enter");
+let reset = document.getElementById("reset");
+let purchaseResult = document.getElementById("result");
+let amount;
+let limit;
+let discount;
+let result;
+
+
+enter.onclick  = function () {
+    amount = purchaseAmount.value;
+    amount = Number(amount);
+    limit = purchaseLimit.value;
+    limit = Number(limit);
+    discount = purchaseDiscount.value;
+    discount = Number(discount);
+
+    result = amount >= limit ? discount : 0;  
+    let formula = amount - amount * (result/100);
+  
+    purchaseResult.textContent = ` Result: Your amount reduce after ${discount}% of discount to pay ₹ ${formula}`;
+
+}
+
+reset.onclick = function () {
+    purchaseAmount.value = '';
+    purchaseLimit.value = '';
+    purchaseDiscount.value = '';
+    purchaseResult.textContent = 'Result:';
+}
